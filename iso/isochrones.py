cbdfcdfaae698a5618f12355fd9efaf26d60d37a
colej <johnston.ccj@gmail.com>
2018-12-19 15:29:38 +0100

    Updated isochrone construction to build M-age relation once

--- iso/isochrones.py
+++ iso/isochrones.py
@@ -13,10 +13,14 @@ def sort_tracks_on_mass(tracks):
     yield tracks
 
 
+def get_keys_dict(keys):
+    key_dict = {key: [] for key in keys}
+    yield key_dict
+
 def make_isochrones_dict(i_ages,keys):
 
-    key_dict = {key: [] for key in keys}
-    isochrones = { 'age-%i'%cc: key_dict for cc,i_age in enumerate(i_ages) }
+    key_dict = list(get_keys_dict(keys))[0]
+    isochrones = { 'age-{}'.format(cc): key_dict for cc,i_age in enumerate(i_ages) }
 
     yield isochrones
 
@@ -24,7 +28,7 @@ def make_isochrones_dict(i_ages,keys):
 def get_subsample(n,tracks,keys):
 
     # eep_subsample = { key: [ track[key][n] for track in tracks  ] for key in keys }
-    eep_subsample = { key: [] for key in keys }
+    eep_subsample = list(get_keys_dict(keys))[0]
     for track in tracks:
         for key in keys:
             eep_subsample[key].append(track[key][n])
@@ -57,16 +61,16 @@ def sort_y_on_x(x,y):
 def loop_over_subsample(i_age,cc,subsample,masses,ages,keys,isochrones):
 
     sorted_ages,sorted_masses = list(sort_y_on_x(ages,masses))
-
-    # plt.plot(sorted_ages,sorted_masses,'r--')
-    # sorted_ages = smooth(np.array(sorted_ages),3,'flat')
-    # plt.plot(sorted_ages,sorted_masses,'k-o')
-    # plt.axvline(i_age,color='blue')
-    # plt.show()
-
     i_age_func = list(make_interpolation_function(sorted_ages,sorted_masses))[0]
     mass_coord = i_age_func(i_age)
     isochrones = list(populate_isochrones(cc,mass_coord,masses,subsample,keys,isochrones))[0]
+
+    plt.plot(sorted_masses,sorted_ages,'k--')
+    plt.axhline(i_age,color='blue')
+    # plt.plot(isochrones['age-{}'.format(cc)]['log_Teff'],isochrones['age-{}'.format(cc)]['log_g'])
+    # plt.xlim(plt.xlim()[::-1])
+    # plt.ylim(plt.ylim()[::-1])
+    # plt.show()
 
     yield isochrones
 
@@ -77,71 +81,79 @@ def populate_isochrones(cc,mass_coord,masses,subsample,keys,isochrones):
         cval = subsample[key]
         sorted_masses,sorted_cval = list(sort_y_on_x(masses,cval))
         i_cval_func = list(make_interpolation_function(sorted_masses,sorted_cval))[0]
+        # isochrones['age-{}'.format(cc)][key].append(i_cval_func(mass_coord))
         isochrones['age-{}'.format(cc)][key].append(i_cval_func(mass_coord))
 
     yield isochrones
+
+def get_tracks_per_eep(tracks,npoints,keys):
+
+    tracks_per_eep = get_keys_dict(keys)
+    # for n_eep in range(npoints):]):
+
 
 
 def construct_isochrones(eep_tracks,i_ages,keys,savename):
 
     #-> Sort tracks based on mass to ensure ease later
     tracks = list(sort_tracks_on_mass(eep_tracks))[0]
+    ntracks = len(tracks)
     npoints = len( tracks[0]['star_age'] )
 
     #-> create a dictionary where each age is a dictionary for all quantities listed above
     isochrones = list(make_isochrones_dict(i_ages,keys))[0]
+    isochrones = {'age-{0:04d}'.format(ii): {key: np.empty(npoints) for key in keys} for ii in range(len(i_ages)) }
+    eep_tracks = {'eep-{}'.format(n):{key: np.empty(ntracks) for key in keys} for n in range(npoints)}
+    eep_mass_age_relations = {'eep-{}'.format(n):{'mass-init': np.empty(ntracks), 'age':np.empty(ntracks)} for n in range(npoints)}
 
-    #-> Loop through all ages and create the isochrone for that age
-    for cc,i_age in enumerate(i_ages):
+    print('--> Building tracks in all quantities per EEP')
+    print('--> Building M_init - Age relation per EEP')
+    for n in range(npoints):
+        for ii,track in enumerate(tracks):
+            eep_mass_age_relations['eep-{}'.format(n)]['mass-init'][ii] = track['star_mass'][0]
+            eep_mass_age_relations['eep-{}'.format(n)]['age'][ii] = track['star_age'][n]
 
-        print('ISOCHRONE FOR: ',i_age/1e6,' Myrs')
+            for key in keys:
+                eep_tracks['eep-{}'.format(n)][key][ii] = track[key][n]
 
-        #-> Loop over each track at the same EEP to generate an isochrone
-        for n_eep in range(npoints):
-
-            eep_subsample,eep_masses,eep_ages = list(get_subsample(n_eep,tracks,keys))
-
-            if ( ( min(eep_ages) < i_age < max(eep_ages) ) & (len(eep_masses) > 3) ) :
-
-                isochrones = list(loop_over_subsample(i_age,cc,eep_subsample,eep_masses,eep_ages,keys,isochrones))[0]
-        # plt.show()
-        #         plt.figure(1)
-        #         plt.plot( eep_masses, np.log10(eep_ages), 'ko-')
-        #         plt.axvline(i_age_func(i_age),color='red',alpha=0.5)
-        #         plt.axhline(np.log10(i_age),color='red',alpha=0.5)
-        #
-        # plt.show()
-
-                # plt.figure(2)
-                # plt.plot(isochrones['age-%i'%cc]['log_Teff'],isochrones['age-%i'%cc]['log_g'],'k-',alpha=0.7)
-
-    for cc,i_age in enumerate(i_ages):
+    print('\t--> All tracks and M_init - Age relations built')
+    print('--> Creating interpolation functions for all tracks and M_init - Age relations')
+    interpolated_mass_age_relations = {}
+    interpolated_eep_tracks = {'eep-{}'.format(n): { key: [] for key in keys} for n in range(npoints)}
+    for n in range(npoints):
+        x = eep_mass_age_relations['eep-{}'.format(n)]['age']
+        y = eep_mass_age_relations['eep-{}'.format(n)]['mass-init']
+        x,y = list(sort_y_on_x(x,y))
+        interpolated_mass_age_relations['eep-{}'.format(n)] = Akima1DInterpolator(x,y)
         for key in keys:
-            isochrones['age-%i'%cc][key] = np.hstack(isochrones['age-%i'%cc][key])
+            xx = eep_tracks['eep-{}'.format(n)]['star_mass']
+            xy = eep_tracks['eep-{}'.format(n)][key]
+            xx,xy = list(sort_y_on_x(xx,xy))
+            ai_func = Akima1DInterpolator(xx,xy)
+            interpolated_eep_tracks['eep-{}'.format(n)][key] = ai_func
+    print('\t--> All interpolation functions constructed')
 
-    # plt.figure(2)
-    # plt.ylim(plt.ylim()[::-1])
-    #
-    # plt.show()
+    print('--> Looping over all ages to interpolate isochrones')
+    for ii,i_age in enumerate(i_ages):
+        print('Constructing isochrone at {:.1f} Myr'.format(i_age*1e-6))
+        mass_values = [ interpolated_mass_age_relations['eep-{}'.format(n)](i_age) for n in range(npoints) ]
+        for key in keys:
+            for n,m_val in enumerate(mass_values):
+                isochrones['age-{0:04d}'.format(ii)][key][n] = interpolated_eep_tracks['eep-{}'.format(n)][key](m_val)
+        plt.plot(isochrones['age-{0:04d}'.format(ii)]['log_Teff'],isochrones['age-{0:04d}'.format(ii)]['log_g'],'r--')
+    print('\t--> All isochrones constructed')
+    plt.ylim(plt.ylim()[::-1])
+    plt.xlim(plt.xlim()[::-1])
+    plt.show()
+
+
 
     with open(savename,'w') as fout:
         header = '# AGE[Myr]  %s'%' '.join( [ '%s'%key for key in keys ] )
         fout.write(header+'\n')
-        for cc,i_age in enumerate(i_ages):
-            for n in range(len(isochrones['age-%i'%cc]['star_mass'])):
+        for ii,i_age in enumerate(i_ages):
+            for n in range(len(isochrones['age-{0:04d}'.format(ii)]['star_mass'])):
                 # print(n,'/',len(isochrones['age-%i'%cc]['star_mass']))
-                fout.write( '%f %s \n'%(i_age,' '.join( [ '%.8f'%isochrones['age-%i'%cc][key][n] for key in keys] )) )
-
-
-
-
-            #valid_eeps.append(np.hstack(valid_masses))
-
-    #for mass in masses:
-        #plt.axvline(mass,color='red',alpha=0.5)
-    #for i_age in i_ages:
-        #plt.axhline(np.log10(i_age),color='blue',alpha=0.5)
-
-    #plt.show()
+                fout.write( '%f %s \n'%(i_age,' '.join( [ '%.8f'%isochrones['age-{0:04d}'.format(ii)][key][n] for key in keys] )) )
 
     return
